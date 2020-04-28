6536338fc5de9de5350893557e63f48a42e72443
sharon k <kiwaiko@gmail.com>
2020-04-29 00:11:15 +0300

    update

--- destructure/destructuring.js
+++ destructure/destructuring.js
@@ -7,13 +7,13 @@ const movie = {
 
 
 // Initialize local variables
-let title = '';
+let title = 'empty';
 let ctg = '';
 let year = '';
 // Reassign ctg and lastname using destructuring
 // Enclose in a pair of parentheses, since this is an assignment expression
 ({ ctg, year } = movie);
 
-// country remains unchanged (Canada)
-console.log(title, ctg, year); // The Invisible Guest Mystery 2016
+// country remains unchanged (empty)
+console.log(title, ctg, year); // empty Mystery 2016
 //node destructuring
